9d452df6417f9550f7b12e2ae26cd7e179229901
unknown <somayaelbaradey@gmail.com>
2020-10-26 12:35:58 +0200

    add lab 6

--- Lab6/lineEditor/main.c
+++ Lab6/lineEditor/main.c
@@ -21,35 +21,48 @@ void gotoxy( int column, int line )
     coord.Y = line;
     SetConsoleCursorPosition(GetStdHandle( STD_OUTPUT_HANDLE ),coord);
 }
+void swap(char* a, char* b)
+{
+    char tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
 int main()
 {
     int flag =1;
     char line[31];
-    char pfirst, plast, pcurrent;
+    char *pfirst, *plast, *pcurrent;
     char ch;
     int currentpos, lastpos, firstpos;
     currentpos=firstpos=lastpos=10;
-    pfirst=plast=pcurrent;
-    for (int i=0; i<31; i++){
+    pfirst=plast=pcurrent=line;
+    for (int i=0; i<31; i++)
+    {
         gotoxy(10+i, 10);
         textattr(23);
         _cprintf(" ");
     }
-    do{
+
+    do
+    {
         gotoxy(currentpos,10);
         ch=getch();
-        switch(ch){
+        switch(ch)
+        {
         case Extended:
             ch =getch();
-            switch(ch){
+            switch(ch)
+            {
             case left:
-                if(currentpos>firstpos){
+                if(currentpos>firstpos)
+                {
                     currentpos--;
                     pcurrent--;
                 }
                 break;
             case right:
-                if(currentpos<lastpos){
+                if(currentpos<lastpos)
+                {
                     currentpos++;
                     pcurrent++;
                 }
@@ -63,6 +76,28 @@ int main()
                 pcurrent=pfirst;
                 break;
             case delete:
+                if(currentpos>=firstpos&&currentpos<lastpos)
+                {
+                    int pos=currentpos;
+                    int ptr=pcurrent;
+                    pcurrent++;
+                    currentpos++;
+                    lastpos--;
+                    plast--;
+                    *pcurrent=*(pcurrent+1);
+                    gotoxy(currentpos,10);
+                    printf("%c",*pcurrent);
+                    //shifting
+                    for(int i=0; i<(lastpos-currentpos)+1; i++)
+                    {
+                        pcurrent++;
+                        currentpos++;
+                        *pcurrent=*(pcurrent+1);
+                        printf("%c",*pcurrent);
+                    }
+                    currentpos=pos;
+                    pcurrent=ptr;
+                }
                 break;
             }
             break;
@@ -73,21 +108,70 @@ int main()
             printf("%s",line);
             break;
         case backSpace:
+            if(currentpos>firstpos)
+            {
+                if(currentpos==lastpos)
+                {
+                    currentpos--;
+                    pcurrent--;
+                    lastpos--;
+                    plast--;
+                    gotoxy(currentpos,10);
+                    printf("%c",' ');
+                    *pcurrent=' ';
+                }
+                else
+                {
+                    //shifting
+                    currentpos--;
+                    pcurrent--;
+                    *pcurrent=*(pcurrent+1);
+                    gotoxy(currentpos,10);
+                    printf("%c",*pcurrent);
+                    int num = lastpos-currentpos;
+                    //printf("%d",num);
+                    for(int i=0; i<(lastpos-currentpos)+1; i++)
+                    {
+                        pcurrent++;
+                        currentpos++;
+                        *pcurrent=*(pcurrent+1);
+                        printf("%c",*pcurrent);
+                    }
+                    if(num!=0)
+                    {
+                        currentpos-=num-1;
+                        pcurrent-=num-1;
+                    }
+                }
+            }
             break;
         default:
-            if(isprint(ch)){
-                if(currentpos<firstpos+30){
-                    if(pcurrent==plast){
+            _flushall();
+            if(isprint(ch))
+            {
+                if(currentpos<firstpos+30)
+                {
+                    if(currentpos==lastpos)
+                    {
                         *pcurrent=ch;
                         printf("%c",ch);
-                        pcurrent++; plast++; lastpos++; currentpos++;
-                    } else{
-                        *pcurrent
+                        pcurrent++;
+                        plast++;
+                        lastpos++;
+                        currentpos++;
+                    }
+                    else
+                    {
+                        *pcurrent=ch;
+                        printf("%c",ch);
+                        currentpos++;
+                        pcurrent++;
                     }
                 }
             }
             break;
         }
-    }while(flag==1);
+    }
+    while(flag==1);
     return 0;
 }
