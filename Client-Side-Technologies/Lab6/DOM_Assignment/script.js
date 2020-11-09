a7b9b4041820c5ba8f3dd5f5c995449a5d7e532b
unknown <somayaelbaradey@gmail.com>
2020-11-09 17:20:19 +0200

    modify text style

--- Client-Side-Technologies/Lab6/DOM_Assignment/script.js
+++ Client-Side-Technologies/Lab6/DOM_Assignment/script.js
@@ -1,15 +1,20 @@
-var body = document.children[0].children[1];
-var img= document.getElementById("header").children[0];
-var newImg = img.cloneNode(false);
-//styling
-img.style.position = "absolute"; 
+var img = document.getElementById("header").children[0];
+var newImg = img.cloneNode(true);
+//styling the images
+img.style.position = "absolute";
 img.style.display = "block";
-img.style.width = "25%"; 
-img.style.right = "0%"; 
-img.style.top = "0%"; 
-newImg.style.position = "absolute"; 
+img.style.right = "0%";
+img.style.top = "0%";
+newImg.style.position = "absolute";
 newImg.style.display = "block";
-newImg.style.left = "0%"; 
-newImg.style.top = "0%"; 
+newImg.style.left = "0%";
+newImg.style.bottom = "0%";
 //append 
 document.getElementById("header").appendChild(newImg);
+//modify the list
+var list = document.getElementById("navigation");
+list.style.position = "absolute";
+list.style.marginTop = "300";
+list.style.marginLeft = "50%";
+list.style.marginRight = "50%";
+document.getElementById("nav").style.listStyleType="circle";
---
+++ Client-Side-Technologies/Lab6/TextStyle/script.js
@@ -0,0 +1,28 @@
+var paragraph= document.getElementById("PAR");
+function ChangeFont(value){
+    paragraph.style.fontFamily=value;
+}
+function ChangeAlign(value){
+    paragraph.style.textAlign=value;
+}
+function ChangeHeight(value){
+    paragraph.style.lineHeight=value;
+}
+function ChangeLSpace(value){
+    paragraph.style.letterSpacing=value;
+}
+function ChangeIndent(value){
+    paragraph.style.textIndent=value;
+}
+function ChangeTransform(value){
+    paragraph.style.textTransform=value;
+}
+function ChangeDecorate(value){
+    paragraph.style.textDecoration=value;
+}
+function ChangeBorder(value){
+    paragraph.style.border=value;
+}
+function ChangeBorderColor(value){
+    paragraph.style.borderColor=value;
+}
