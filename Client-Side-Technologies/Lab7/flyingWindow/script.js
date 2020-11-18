783cb5a7a3ad2f9405d30867ae7190f8dcc5774f
unknown <somayaelbaradey@gmail.com>
2020-11-18 21:00:18 +0200

    add lab7

--- Client-Side-Technologies/Lab7/flyingWindow/script.js
+++ Client-Side-Technologies/Lab7/flyingWindow/script.js
@@ -1,53 +1,12 @@
 var myWindow = window.open("", "", "width=200,height=100");
-var opened=1;
-var down;
-var up;
-
 document.getElementById("stop").addEventListener("click", function () {
-    //opened = 0;
-    myWindow.close()    
+    clearInterval(opened);
+    myWindow.close();
 })
-
-// do {
-//     for (var i = 0; i < window.innerHeight; i++) {
-//         myWindow.moveBy(1, 1);
-//     } for (var i = 0; i < window.innerHeight; i++) {
-//         myWindow.moveBy(-1, -1);
-//     }
-// } while (open == 1);
-
-// function show() {
-//     down = setTimeout(function () {
-//         for (var i = 0; i < window.innerHeight; i++) {
-//             myWindow.moveBy(1, 1);
-//         }
-        
-//     }, 100);
-//     up = setTimeout(function () {
-//         for (var i = 0; i < window.innerHeight; i++) {
-//             myWindow.moveBy(-1, -1);
-//         }
-//     }, 100);
-// }
-
-setInterval(function() {
-    down = setTimeout(function () {
-        for (var i = 0; i < window.innerHeight; i++) {
-            myWindow.moveBy(1, 1);
-        }
-    }, 100);
-    up = setTimeout(function () {
-        for (var i = 0; i < window.innerHeight; i++) {
-            myWindow.moveBy(-1, -1);
-        }
-    }, 100);
-    clearTimeout(dowm);
-    clearTimeout(up);
-}, 1000);
-
-// for(var i=0; i<3; i++){
-//     show();
-// }
-
-///modify ASCII 
-//Flying Window
+var opened = setInterval(function(){
+    for (var i = 0; i < window.innerHeight; i++) {
+        myWindow.moveBy(1, 1);
+    } for (var i = 0; i < window.innerHeight; i++) {
+        myWindow.moveBy(-1, -1);
+    }
+}, 500)
